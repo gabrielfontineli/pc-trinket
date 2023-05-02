76288bbce6e9c1416e215b68f9c4da1a67973c7f
gabrielfontineli <gabriel0fontineli@gmail.com>
2023-05-02 18:07:31 -0300

    fix question 5

--- lista03/questao05.py
+++ lista03/questao05.py
@@ -1,8 +1,7 @@
-meses = ["Janeiro","Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Novembro", "Dezembro"]
+meses = ["Janeiro","Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
 
 entrada = int(input("Qual o número do mês?"))
-entrada -= 1
-if meses[entrada] in meses:
-    print(f"O mês é {meses[entrada]}")
+if entrada > 0 and entrada < 13:
+    print(f"O mês é {meses[entrada-1]}")
 else:
     print(f"Não existe mes de número {entrada}")
